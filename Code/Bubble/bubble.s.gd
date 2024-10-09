b809c7bcb63cc309b9df7bdd2a19d1149e799ac7
user0handyman <ethan.beickel@pcc.edu>
2024-10-09 15:49:39 -0700

    Clicking operational.

--- Code/Bubble/bubble.s.gd
+++ Code/Bubble/bubble.s.gd
@@ -1,9 +1,21 @@
-extends Node2D
+extends Area2D
 class_name Bubble
 
-
+func _ready() -> void:
+	input_event.connect(OnClicked)
+	
 	
 func _process(delta: float) -> void:
 	position.y -= 100 * delta
 	if position. y < 0:
 		queue_free()
+
+func OnClicked(veiwport, event, shape_id):
+	if event is InputEventMouseButton:
+		if event.pressed:
+			if event.button_index == MOUSE_BUTTON_MASK_RIGHT:
+				print("click")
+			Pop()
+
+func Pop():
+	queue_free()
