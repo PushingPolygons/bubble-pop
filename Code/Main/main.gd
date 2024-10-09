30ae7c61e00d02f38dce86fd4083ef2c1e76f92c
piaculum <loren.bayouth@pcc.edu>
2024-10-09 16:47:57 -0700

    Bubble pop + bubble pop sound

--- Code/Main/main.gd
+++ Code/Main/main.gd
@@ -3,6 +3,7 @@ class_name Main
 
 const BUBBLE = preload("res://Bubble/Bubble.tscn")
 @onready var timer: Timer = $Timer
+@onready var audio_player: AudioStreamPlayer2D = $AudioPlayer
 
 func _ready() -> void:
 	timer.timeout.connect(OnTimedOut)
@@ -10,11 +11,13 @@ func _ready() -> void:
 
 func OnTimedOut():
 	SpawnBubble()
+	#audio_player.play()
 
 func SpawnBubble():
 	var bubble_instance = BUBBLE.instantiate()
+	bubble_instance.main = self
 	bubble_instance.position = Vector2(randf_range(0, get_viewport().size.x), get_viewport().size.y) 
 	add_child(bubble_instance)
  
-func _process(delta: float) -> void:
-	pass
+func PlaySound():
+	audio_player.play()
