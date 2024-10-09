8039449ab3d54044691bf8d054c919779dedfd89
Skythesiren <skyler.cainhanson@pcc.edu>
2024-10-09 15:04:18 -0700

    Added pop code to bubble

--- Code/Bubble/Bubble.gd
+++ Code/Bubble/Bubble.gd
@@ -1,7 +1,17 @@
-extends Node2D
+extends Area2D
 class_name Bubble
 
 var spd: int = 100
 
+func _ready():
+	mouse_entered.connect(Pop_Bubble)
+
 func _process(delta: float) -> void:
 	position.y -= spd * delta
+	#if Input.is_action_just_pressed("Pop"):
+		#mouse_entered.connect(Pop_Bubble)
+	if position.y < -50:
+		queue_free();
+func Pop_Bubble():
+	if Input.is_action_just_pressed("Pop"):
+		queue_free();
