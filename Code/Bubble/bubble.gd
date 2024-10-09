89d1ad29fb2af2c274dd15927c32de2bf5fff922
AesonR <aeson.ranspot@pcc.edu>
2024-10-09 15:46:35 -0700

    clicking bubbles

--- Code/Bubble/bubble.gd
+++ Code/Bubble/bubble.gd
@@ -1,9 +1,18 @@
-extends Node2D
+extends Area2D
 class_name Bubble
+
+func Pop():
+	queue_free()
+
+func OnClicked(viewport, event, shape_index):
+	if event is InputEventMouseButton:
+		if event.pressed:
+			if event.button_index == MOUSE_BUTTON_LEFT:
+				Pop()
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	input_event.connect(OnClicked)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
