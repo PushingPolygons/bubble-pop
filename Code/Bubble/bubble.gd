b3b2ca2d5ca58bce435365f3bebeec6b3656b2b9
theuserguy2 <maxim.tarai@pcc.edu>
2024-10-09 15:46:07 -0700

    clicking kills

--- Code/Bubble/bubble.gd
+++ Code/Bubble/bubble.gd
@@ -1,5 +1,18 @@
-extends Node2D
+extends Area2D
 class_name Bubble
+
+func _ready() -> void:
+	input_event.connect(OnClick)
+
+func OnClick(viewport: Node, event, shape_id):
+	#if(event.)
+	if event is InputEventMouseButton:
+		if event.pressed:
+			if event.button_index == MOUSE_BUTTON_LEFT:
+				Pop();
+
+func Pop():
+	queue_free();
 
 func _process(delta: float) -> void:
 	position.y -= 100 * delta
