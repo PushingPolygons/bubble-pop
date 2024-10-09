a83cf4924e1d937678bc08e0bc4cbd7824634425
theuserguy2 <maxim.tarai@pcc.edu>
2024-10-09 16:42:51 -0700

    som som

--- Code/Bubble/bubble.gd
+++ Code/Bubble/bubble.gd
@@ -1,20 +1,45 @@
 extends Area2D
 class_name Bubble
 
+var shake: bool;
+var killtimer: float;
+
+var main: Main;
+
 func _ready() -> void:
+	killtimer = 0;
 	input_event.connect(OnClick)
+	
 
 func OnClick(viewport: Node, event, shape_id):
 	#if(event.)
+	
 	if event is InputEventMouseButton:
 		if event.pressed:
 			if event.button_index == MOUSE_BUTTON_LEFT:
 				Pop();
+	else:
+		shake = true;
+		
 
 func Pop():
-	queue_free();
+	killtimer = .2;
+	#queue_free();
 
 func _process(delta: float) -> void:
+	if (killtimer < 0):
+		main.PlaySound()
+		queue_free()
+	if(killtimer > 0):
+		scale.x += delta * 5;
+		scale.y += delta * 5;
+		killtimer -= delta;
+		
+	if shake:
+		position.x += randf_range(-100,100) * delta;
+		position.y += randf_range(-100,100) * delta;
+		shake = false;
+
 	position.y -= 100 * delta
-	if position.y < 0:
+	if position.y < -50:
 		queue_free();
--- Code/Main/main.gd
+++ Code/Main/main.gd
@@ -3,15 +3,20 @@ class_name Main
 
 const BUBBLE = preload("res://Bubble/Bubble.tscn")
 @onready var timer: Timer = $Timer
+@onready var audio_stream_player_2d: AudioStreamPlayer2D = $AudioStreamPlayer2D
 
 func _ready() -> void:
 	timer.timeout.connect(OnTimedOut)
-	timer.wait_time = 0.25
+	timer.wait_time = 0.24
 
 func OnTimedOut():
 	SpawnBubble()
 
+func PlaySound():
+	audio_stream_player_2d.play(0);
+
 func SpawnBubble():
 	var bubble_instance = BUBBLE.instantiate()
-	bubble_instance.position = Vector2(randf_range(0, get_viewport().size.x), get_viewport().size.y)
+	bubble_instance.main = self;
+	bubble_instance.position = Vector2(randf_range(0, get_viewport().size.x), get_viewport().size.y*1.1)
 	add_child(bubble_instance)
