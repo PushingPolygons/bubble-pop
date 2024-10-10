bd4675e177b080ce4d77087868d0d3ad7316d203
unblinky <>
2024-10-09 23:02:22 -0700

    Playing sounds.

--- Code/Main/main.gd
+++ Code/Main/main.gd
@@ -3,15 +3,21 @@ class_name Main
 
 const BUBBLE = preload("res://Bubble/Bubble.tscn")
 @onready var timer: Timer = $Timer
+@onready var audio_player: AudioStreamPlayer2D = $AudioPlayer
 
 func _ready() -> void:
 	timer.timeout.connect(OnTimedOut)
 	timer.wait_time = 0.25
 
+func PlaySound():
+	audio_player.play()
+
 func OnTimedOut():
 	SpawnBubble()
+	#audio_player.play()
 
 func SpawnBubble():
 	var bubble_instance = BUBBLE.instantiate()
+	bubble_instance.main = self
 	bubble_instance.position = Vector2(randf_range(0, get_viewport().size.x), get_viewport().size.y)
 	add_child(bubble_instance)
