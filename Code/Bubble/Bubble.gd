bbb724f3eab62217a9d2dd49ce752235e01c6e4c
Skythesiren <skyler.cainhanson@pcc.edu>
2024-10-09 17:02:33 -0700

    Make up your mind

--- Code/Bubble/Bubble.gd
+++ Code/Bubble/Bubble.gd
@@ -7,20 +7,17 @@ var id: int
 
 func _ready() -> void:
 	randomize()
-	id = randf()
+	id = randi()
 	input_event.connect(OnClicked);
 
 func _process(delta: float) -> void:
 	position.y -= spd * delta
 	if position.y < -50:
 		queue_free();
-func OnClicked(viewport, event, shape_id = randi()):
+func OnClicked(viewport, event, shape_id):
 	if event is InputEventMouseButton:
 		if Input.is_action_pressed("Pop"):
-			print(str(shape_id))
 			Pop()
-		else:
-			return
 func Pop():
 	main.PlaySound()
 	queue_free();
