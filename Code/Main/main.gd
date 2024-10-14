7c6ee3c6687bec288c895e3808cd1384d178b632
Dreyydoedub <drewaters89@gmail.com>
2024-10-14 15:19:15 -0700

    Score Board

--- Code/Main/main.gd
+++ Code/Main/main.gd
@@ -3,17 +3,22 @@ class_name Main
 
 const BUBBLE = preload("res://Bubble/bubble.tscn")
 @onready var timer: Timer = $Timer
+@onready var audio_player: AudioStreamPlayer2D = $AudioPlayer
 	
 func _ready() -> void:
 	#SpawnBubble()
 	timer.timeout.connect(OnTimedOut)
 	timer.wait_time = 0.25
 
+func PlaySound():
+	audio_player.play()
+
 func OnTimedOut():
 	SpawnBubble()
-
+	#audio_player.play()
 
 func SpawnBubble(): 
 	var bubble_instance = BUBBLE.instantiate()
+	bubble_instance.main = self
 	bubble_instance.position = Vector2(randf_range(0,get_viewport().size.x), get_viewport().size.y)
 	add_child(bubble_instance)
